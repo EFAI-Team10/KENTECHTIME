f2d8b1f3ace1462870c463b75ad597756423d69d
miniipad03 <miniipad03@kentech.ac.kr>
2026-05-28 10:28:34 +0900

    feat(client): wrap App in GoogleOAuthProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import './index.css';
+
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+if (!clientId) {
+  // eslint-disable-next-line no-console
+  console.error('REACT_APP_GOOGLE_CLIENT_ID is not set. Google login will not work.');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId || ''}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
